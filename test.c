74ba69a0cc6bff18908e3ae1014cffedb59513bd
Ananya Lohani <ananya19018@iiitd.ac.in>
2020-10-23 21:07:32 +0530

    added test.c

--- test.c
+++ test.c
@@ -1,2 +1,25 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* 
+   Name: Ananya Lohani
+   Roll_Number: 2019018 
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+   pid_t pid = (pid_t)atoi(argv[1]);
+   long res = syscall(548, pid, argv[2]);
+   if(res == 0)
+   {
+      printf("System call successful.");
+   }
+   else 
+   {
+      perror("Error: ");
+      fprintf(stderr, "Error number: %d\n", errno);
+   }
+   return 0;
+}
